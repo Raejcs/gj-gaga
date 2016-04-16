987a79d17be84cb5d0efd8fc8358064e7b14f5df
voze <victor@forsgrens.nu>
2016-04-16 14:06:19 +0200

    Such Coding, Such wow

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -3,28 +3,41 @@ using System.Collections;
 
 public class movement : MonoBehaviour {
     public float speed = 10f;
+    private float prevSpeed;
 	// Use this for initialization
 	void Start () {
-	
+        prevSpeed = speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = prevSpeed * 2;
+        }
+        else
+        {
+            speed = prevSpeed;
+        }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+            GetComponent<Rigidbody>().MovePosition(transform.position + -transform.forward * Time.deltaTime * speed);
+            //transform.Translate(-Vector3.forward * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-Vector3.right * Time.deltaTime * speed);
+            GetComponent<Rigidbody>().MovePosition(transform.position + -transform.right * Time.deltaTime * speed);
+            //transform.Translate(-Vector3.right * Time.deltaTime * speed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * speed);
+            GetComponent<Rigidbody>().MovePosition(transform.position + transform.right * Time.deltaTime * speed);
+            //transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
 
     }
